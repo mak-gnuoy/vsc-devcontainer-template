33a49934bdbbbcd5b5351a2c40a61dd8a94a8323
mak-gnuoy <mak.gnuoy@gmail.com>
2026-05-10 18:12:19 +0900

    fix: invalid scripts

--- docker/create.appuser.sh
+++ docker/create.appuser.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <username>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <username> <app_path>"
     exit 1
 fi
 
 USERNAME="$1"
+APP_PATH="$2"
 
 if [ -f /etc/os-release ]; then
     source /etc/os-release
     if [[ $ID == "rhel" || $ID == "centos" || $ID == "fedora" || $ID_LIKE =~ "rhel" ]]; then
-        # Red Hat 계열
-        useradd -u 5678 -m -s /bin/bash "$USERNAME"
+        # Red Hat family
+        useradd -u 5678 -m -s /bin/bash "$USERNAME" && chown -R "$USERNAME" "$APP_PATH"
     elif [[ $ID == "debian" || $ID == "ubuntu" || $ID_LIKE =~ "debian" ]]; then
-        # Debian 계열
-        adduser -u 5678 --disabled-password --gecos "" "$USERNAME"
+        # Debian family
+        adduser -u 5678 --disabled-password --gecos "" "$USERNAME" && chown -R "$USERNAME" "$APP_PATH"
     else
         echo "Unsupported OS: $ID"
         exit 1
@@ -23,4 +24,3 @@ else
     echo "/etc/os-release not found"
     exit 1
 fi
-
